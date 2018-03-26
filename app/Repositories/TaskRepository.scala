6890f8a0055a45240e82c019bb0af60716deaa7d
Panithan Sophonthammakhun <panithan.s@scale360solutions.com>
2018-03-26 16:58:28 +0700

    binding database table with model class

--- app/Repositories/TaskRepository.scala
+++ app/Repositories/TaskRepository.scala
@@ -1,11 +1,34 @@
 package Repositories
 
 import javax.inject.{Inject, Singleton}
+import models.Task
 import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{ExecutionContextExecutor, Future}
 
 @Singleton
 class TaskRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContextExecutor) {
 
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import profile.api._
+
+  private class TaskTable(tag: Tag) extends Table[Task](tag, "task") {
+
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+    def name = column[String]("name")
+    def detail = column[String]("detail")
+    def done = column[Boolean]("done")
+
+    def * = (id, name, detail, done) <> ((Task.apply _).tupled, Task.unapply)
+  }
+
+  private val task = TableQuery[TaskTable]
+
+  def list(): Future[Seq[Task]] = db.run {
+    task.result
+  }
 }
--- app/controllers/TaskController.scala
+++ app/controllers/TaskController.scala
@@ -1,7 +1,15 @@
 package controllers
 
 import Repositories.TaskRepository
-import javax.inject.Inject
+
+import javax.inject._
+
+import models._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.i18n._
+import play.api.libs.json.Json
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,5 +19,20 @@ class TaskController @Inject() (repo: TaskRepository,
                                )(implicit ec: ExecutionContext)
 extends AbstractController (cc) {
 
+  val taskForm: Form[CreateTaskForm] = Form {
+    mapping(
+      "name" -> nonEmptyText,
+      "detail" -> nonEmptyText,
+      "done" -> boolean
+    )(CreateTaskForm.apply)(CreateTaskForm.unapply)
+  }
+
+  def index = Action.async {implicit request =>
+    repo.list().map { people =>
+      Ok(Json.toJson(people))
+    }
+  }
 
 }
+
+case class CreateTaskForm(name: String, detail: String, done: Boolean)
